eb5a55cd833e58e7c6c6f7201da554cef518b151
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-12-06 10:40:56 +0600

    Initialized Root Layout

---
+++ src/routes/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
+  }
+])
