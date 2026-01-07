2678b92cd39b512c6de52c63c038d9e906558a05
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-01-07 20:32:03 +0600

    Added CountUp to Story

--- src/pages/home/Story.jsx
+++ src/pages/home/Story.jsx
@@ -10,6 +10,7 @@ import gal5 from '../../assets/gal5.avif';
 import gal6 from '../../assets/gal6.avif';
 
 import { motion } from "framer-motion";
+import CountUp from "react-countup";
 
 import 'swiper/css';
 
@@ -19,42 +20,86 @@ const Story = () => {
   return (
     <div className='max-w-7xl mx-auto py-20 mb-10'>
       <div className='flex flex-col md:flex-row gap-10'>
-        <motion.div className='flex-1' initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}>
+        <motion.div
+          className='flex-1'
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6 }}
+        >
           <div className='flex justify-center gap-10'>
             <section className='flex flex-col items-center hover:scale-110'>
-              <h1 className='font-black text-center text-2xl md:text-5xl'>50+</h1>
-              <p className='text-primary text-center text-xs md:text-lg font-bold'>Blood Cooperations</p>
+              <h1 className='font-black text-center text-2xl md:text-5xl'>
+                <CountUp end={50} duration={2} enableScrollSpy />+
+              </h1>
+              <p className='text-primary text-center text-xs md:text-lg font-bold'>
+                Blood Cooperations
+              </p>
             </section>
+
             <section className='flex flex-col items-center hover:scale-110'>
-              <h1 className='font-black text-center text-2xl md:text-5xl'>100+</h1>
-              <p className='text-primary text-center text-xs md:text-lg font-bold'>Expert Volunteers</p>
+              <h1 className='font-black text-center text-2xl md:text-5xl'>
+                <CountUp end={100} duration={2} enableScrollSpy />+
+              </h1>
+              <p className='text-primary text-center text-xs md:text-lg font-bold'>
+                Expert Volunteers
+              </p>
             </section>
+
             <section className='flex flex-col items-center hover:scale-110'>
-              <h1 className='font-black text-center text-2xl md:text-5xl'>500+</h1>
-              <p className='text-primary text-center text-xs md:text-lg font-bold'>Blood Donations</p>
+              <h1 className='font-black text-center text-2xl md:text-5xl'>
+                <CountUp end={500} duration={2} enableScrollSpy />+
+              </h1>
+              <p className='text-primary text-center text-xs md:text-lg font-bold'>
+                Blood Donations
+              </p>
             </section>
           </div>
 
           <div className='flex gap-5 px-5 md:px-0 mt-10'>
-            <motion.section className='w-1/2 py-10 px-5 md:p-10 bg-primary md:h-80 text-base-200 flex flex-col rounded overflow-hidden' initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, delay: 0.2 }}>
+            <motion.section
+              className='w-1/2 py-10 px-5 md:p-10 bg-primary md:h-80 text-base-200 flex flex-col rounded overflow-hidden'
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.2 }}
+            >
               <div className='transition-transform duration-300 ease-out hover:scale-110'>
-                <h1 className='text-xs md:text-4xl font-black'>Why we need you?</h1>
+                <h1 className='text-xs md:text-4xl font-black'>
+                  Why we need you?
+                </h1>
                 <hr className='mt-2 md:mt-5 mb-2 md:mb-5' />
-                <p className='text-xs md:text-sm text-justify line-clamp-3 md:line-clamp-none'>A blood transfusion is required every two seconds. However, less than 5% donate blood. Our goal is to close that gap by making donations easy, secure, and fulfilling.</p>
+                <p className='text-xs md:text-sm text-justify line-clamp-3 md:line-clamp-none'>
+                  A blood transfusion is required every two seconds. However, less than 5% donate blood. Our goal is to close that gap by making donations easy, secure, and fulfilling.
+                </p>
               </div>
             </motion.section>
 
-            <motion.section className='flex-1 overflow-hidden rounded' initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, delay: 0.3 }}>
-              <img src={auth} alt="" className="w-full h-40 md:h-80 object-cover rounded transition-transform duration-300 ease-out hover:scale-120" />
+            <motion.section
+              className='flex-1 overflow-hidden rounded'
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.3 }}
+            >
+              <img
+                src={auth}
+                alt=""
+                className="w-full h-40 md:h-80 object-cover rounded transition-transform duration-300 ease-out hover:scale-120"
+              />
             </motion.section>
           </div>
         </motion.div>
 
         <motion.div
-          className='flex-1 w-full' initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.6 }}>
-          <h1 className='text-primary font-black text-2xl text-center md:text-left mb-2 pl-2'>Our Story</h1>
-          <p className='font-black text-3xl mb-7 text-center md:text-left leading-tight pl-2'>Dedicated to Life,<br />The Story of Our Blood Drive Initiative.</p>
+          className='flex-1 w-full'
+          initial={{ opacity: 0, x: 50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          <h1 className='text-primary font-black text-2xl text-center md:text-left mb-2 pl-2'>
+            Our Story
+          </h1>
+          <p className='font-black text-3xl mb-7 text-center md:text-left leading-tight pl-2'>
+            Dedicated to Life,<br />The Story of Our Blood Drive Initiative.
+          </p>
 
           <Swiper
             modules={[Autoplay]}
@@ -68,7 +113,11 @@ const Story = () => {
           >
             {images.map((img, i) => (
               <SwiperSlide key={i} className="w-full h-80">
-                <img src={img} alt={`slide-${i}`} className="w-[600px] h-72 object-cover transition-transform duration-300 ease-out hover:scale-110" />
+                <img
+                  src={img}
+                  alt={`slide-${i}`}
+                  className="w-[600px] h-72 object-cover transition-transform duration-300 ease-out hover:scale-110"
+                />
               </SwiperSlide>
             ))}
           </Swiper>
